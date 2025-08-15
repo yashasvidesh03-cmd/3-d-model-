2dec3cf624b5f7c9f3b171a751658acf91474f33
yashasvidesh03-cmd <yashasvidesh03@gmail.com>
2025-08-15 20:04:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-document.querySelectorAll(".openCameraBtn").forEach(button => {
-  button.addEventListener("click", () => {
-    const modelUrl = button.getAttribute("data-model");
+function openAR(modelFile) {
+    const modelUrl = `https://yashasvidesh03-cmd.github.io/3-d-model-/${modelFile}`;
     const sceneViewerUrl = `intent://arvr.google.com/scene-viewer/1.0?file=${encodeURIComponent(modelUrl)}&mode=ar_only#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;end;`;
     window.location.href = sceneViewerUrl;
-  });
-});
-
-console.log("3D AR Gallery loaded");
+}
